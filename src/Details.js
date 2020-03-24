817edb7ad8a24a54772122f532683bb58e9c9275
Nick Norris <43795207+100eyez@users.noreply.github.com>
2020-03-23 19:17:40 -0500

    add details route, add Babel config, add Carousel

--- src/Details.js
+++ src/Details.js
@@ -1,14 +1,9 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loading: true
-    };
-  }
+  state = { loading: true };
   componentDidMount() {
     pet.animal(parseInt(this.props.id)).then(({ animal }) => {
       this.setState({
@@ -28,10 +23,12 @@ class Details extends React.Component {
       return <h1>loading ...</h1>;
     }
 
-    const { animal, breed, location, description, name } = this.state;
+    const { animal, breed, location, description, name, media } = this.state;
+
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1> {name} </h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
