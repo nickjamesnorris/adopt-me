6364cf17a0ce6c5f1637f9c3375c4cd4424ae497
Nick Norris <43795207+100eyez@users.noreply.github.com>
2020-04-29 14:26:25 -0500

    Last commit, adopt button implemented

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("peru");
   return (
-    <div>
-      <header>
-        <Link to="/">Adopt Me</Link>
-      </header>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me</Link>
+        </header>
 
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/Details/:id" />
-      </Router>
-    </div>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/Details/:id" />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,14 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import { navigate } from "@reach/router";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 class Details extends React.Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
   componentDidMount() {
-    throw new error("reactgod");
     pet.animal(parseInt(this.props.id)).then(({ animal }) => {
       this.setState({
         url: animal.url,
@@ -20,12 +22,22 @@ class Details extends React.Component {
       });
     }, console.error);
   }
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  adopt = () => navigate(this.state.url);
   render() {
     if (this.state.loading) {
       return <h1>loading ...</h1>;
     }
 
-    const { animal, breed, location, description, name, media } = this.state;
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      media,
+      showModal
+    } = this.state;
 
     return (
       <div className="details">
@@ -33,8 +45,28 @@ class Details extends React.Component {
         <div>
           <h1> {name} </h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No, I'm a monster</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
